e8c5d3c381857ac4e435f881416c88899b3f9902
Simon Schiffer <simon.schiffer@gmx.at>
2021-06-12 10:30:56 +0200

    Commit adapt to new frontend

--- UserService/src/main/java/com/userservice/demo/UserRestController.java
+++ UserService/src/main/java/com/userservice/demo/UserRestController.java
@@ -1,30 +1,46 @@
 package com.userservice.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.userservice.demo.model.User;
 import com.userservice.demo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserRestController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final HttpClient client = HttpClient.newHttpClient();
+
     @CrossOrigin(origins = "*")
-    @GetMapping("/users")
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    @GetMapping("/getUsers")
+    public List<User> getAllUsers(@RequestParam(value = "loggedInUser") String loggedInUser){
+
+        List<User> allUsers = userRepository.findAll();
+        List<User> filteredList = allUsers
+                .stream()
+                .filter((user -> user.getUsername() != null))
+                .filter((user) -> !user.getUsername().equals(loggedInUser))
+                .collect(Collectors.toList());
+        return filteredList;
     }
 
     @CrossOrigin(origins = "*")
     @PostMapping("/validUser")
     public ResponseEntity<User> isValidUser(@RequestBody User loginUser) {
-        System.out.println(loginUser.getUsername());
 
       List<User> user = userRepository.findByUsernameAndPassword(loginUser.getUsername(), loginUser.getPassword());
       if (user.size()==1) {
@@ -39,15 +55,25 @@ public class UserRestController {
     @CrossOrigin(origins = "*")
     @PutMapping("/newUser")
     public ResponseEntity<User> createNewUser(@RequestBody User newUser) {
-        System.out.println(newUser.getFirstName());
-        System.out.println(newUser.getLastName());
-        System.out.println(newUser.getUsername());
-        System.out.println(newUser.getPassword());
 
         List<User> existingUser = userRepository.findByUsername(newUser.getUsername());
         if (existingUser.size()>0) {
             System.out.println("user already exists");
             return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+
+        HttpRequest request = this.buildPutRequest(newUser.getUsername());
+        HttpResponse<String> response = null;
+
+        try{
+            System.out.println("trying to send user to neo4j");
+            response = this.sendRequest(request);
+            int statusCode = response.statusCode();
+            if(statusCode<200 || statusCode>299) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         User user = userRepository.save(newUser);
@@ -57,4 +83,16 @@ public class UserRestController {
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST)
     public long getUser() {return userRepository.count();}
+
+
+    private HttpRequest buildPutRequest(String username) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8081/followservice/newUser"))
+                .PUT(HttpRequest.BodyPublishers.ofString(username))
+                .build();
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 }
--- UserService/src/main/java/com/userservice/demo/config/WebMvcConfig.java
+++ UserService/src/main/java/com/userservice/demo/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:4200")
                 .allowCredentials(false).maxAge(3600);
     }
 }
